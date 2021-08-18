a6da63d74ad758be0307954d5f00b7b81cfecccc
ppoptimus <r.pumpo@gmail.com>
2021-08-18 18:21:52 +0700

    image

--- pages/card.js
+++ pages/card.js
@@ -7,14 +7,14 @@ export default function Card() {
         <li>
           <div className={css.card}>
             <a href="https://watafak88.com/#/register" target="_blank" rel="noreferrer">
-              <img src="https://watafak88.com/img/promotion/pgslot_1-500.jpg" className={css.card__image} alt="pg slot" />
+              <img src="https://watafak88.com/img/promotion/pgslot_1-500.jpg" className={css.card__image} width="50%" height="50%" alt="pg slot" />
             </a>
             <div className={css.card__overlay}>
               <div className={css.card__header}>
                 <svg className={css.card__arc} xmlns="http://www.w3.org/2000/svg">
                   <path />
                 </svg>
-                <img className={css.card__thumb} src="https://watafak88.com/img/menu/logo-nav.png" alt="pg slot" />
+                <img className={css.card__thumb} src="https://watafak88.com/img/menu/logo-nav.png" width="50%" height="50%" alt="pg slot" />
                 <strong>Register</strong>
               </div>
               <p className={css.card__description}>
@@ -26,14 +26,14 @@ export default function Card() {
         <li>
           <div className={css.card}>
             <a href="https://watafak88.com/#/playgame" target="_blank" rel="noreferrer">
-              <img src="https://watafak88.com/img/promotion/pgslot_3-500.jpg" className={css.card__image} alt="pg slot" />
+              <img src="https://watafak88.com/img/promotion/pgslot_3-500.jpg" className={css.card__image} width="50%" height="50%" alt="pg slot" />
             </a>
             <div className={css.card__overlay}>
               <div className={css.card__header}>
                 <svg className={css.card__arc} xmlns="http://www.w3.org/2000/svg">
                   <path />
                 </svg>
-                <img className={css.card__thumb} src="https://watafak88.com/img/menu/logo-nav.png" alt="pg slot" />
+                <img className={css.card__thumb} src="https://watafak88.com/img/menu/logo-nav.png" width="50%" height="50%" alt="pg slot" />
                 <strong>PlayGame</strong>
               </div>
               <p className={css.card__description}>
@@ -45,14 +45,14 @@ export default function Card() {
         <li>
           <div className={css.card}>
             <a href="https://watafak88.com/#/credit" target="_blank" rel="noreferrer">
-              <img src="https://watafak88.com/img/promotion/pgslot_11-500.jpg" className={css.card__image} alt="pg slot" />
+              <img src="https://watafak88.com/img/promotion/pgslot_11-500.jpg" className={css.card__image} width="50%" height="50%" alt="pg slot" />
             </a>
             <div className={css.card__overlay}>
               <div className={css.card__header}>
-                <svg className={css.card__arc} xmlns="https://watafak88.com/img/menu/logo-nav.png">
+                <svg className={css.card__arc} xmlns="http://www.w3.org/2000/svg">
                   <path />
                 </svg>
-                <img className={css.card__thumb} src="https://watafak88.com/img/menu/logo-nav.png" alt="pg slot" />
+                <img className={css.card__thumb} src="https://watafak88.com/img/menu/logo-nav.png" width="50%" height="50%" alt="pg slot" />
                 <strong>Credit Free</strong>
               </div>
               <p className={css.card__description}>แจกเครดิตฟรี 30 บาท ทำตามขั้นตอนง่ายๆ เพียงแค่ 3 ขั้นตอน**</p>
@@ -62,14 +62,14 @@ export default function Card() {
         <li>
           <div className={css.card}>
             <a href="https://pgslot-free.com/" target="_blank" rel="noreferrer">
-              <img src="https://watafak88.com/img/promotion/pgslot_5-500.jpg" className={css.card__image} alt="pg slot" />
+              <img src="https://watafak88.com/img/promotion/pgslot_5-500.jpg" className={css.card__image} width="50%" height="50%" alt="pg slot" />
             </a>
             <div className={css.card__overlay}>
               <div className={css.card__header}>
                 <svg className={css.card__arc} xmlns="http://www.w3.org/2000/svg">
                   <path />
                 </svg>
-                <img className={css.card__thumb} src="https://watafak88.com/img/menu/logo-nav.png" alt="pg slot" />
+                <img className={css.card__thumb} src="https://watafak88.com/img/menu/logo-nav.png" width="50%" height="50%" alt="pg slot" />
                 <strong>Game Free</strong>
               </div>
               <p className={css.card__description}>
