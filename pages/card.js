0092d39711246dc0b2e3ea0c0599274a8fe831ad
ppoptimus <r.pumpo@gmail.com>
2021-08-19 11:03:27 +0700

    add card image

--- pages/card.js
+++ pages/card.js
@@ -1,4 +1,10 @@
 import Image from "next/image";
+import pgslot1 from "../public/img/pgslot_1-500.jpg";
+import pgslot2 from "../public/img/pgslot_3-500.jpg";
+import pgslot3 from "../public/img/pgslot_11-500.jpg";
+import pgslot4 from "../public/img/pgslot_5-500.jpg";
+import pgslot5 from "../public/img/download-pgslot.png";
+import pgslot6 from "../public/img/technique-pg-slots.jpg";
 import css from "../styles/card.module.css";
 
 export default function Card() {
@@ -9,13 +15,13 @@ export default function Card() {
           <div className={css.flipcard}>
             <div className={css.flipcardinner}>
               <div className={css.flipcardfront}>
-                <img src="https://watafak88.com/img/promotion/pgslot_1-500.jpg" alt="pg slot" width="100%" height="100%" style={{ borderRadius: "30px" }} />
+                <Image src={pgslot1} width={200} height={200} alt="pg slot" className={css.flipcardfront} />
               </div>
-              <div className={css.flipcardback}>
-                <h1>John Doe</h1>
-                <p>Architect &amp; Engineer</p>
-                <p>We love that guy</p>
-              </div>
+              <a href="https://watafak88.com/#/register" target="_blank" rel="noopener noreferrer">
+                <div className={css.flipcardback}>
+                  <h1>สมัครใหม่</h1>
+                </div>
+              </a>
             </div>
           </div>
         </div>
@@ -24,13 +30,13 @@ export default function Card() {
           <div className={css.flipcard}>
             <div className={css.flipcardinner}>
               <div className={css.flipcardfront}>
-                <img src="https://watafak88.com/img/promotion/pgslot_3-500.jpg" alt="pg slot" width="100%" height="100%" style={{ borderRadius: "30px" }} />
+                <Image src={pgslot2} width={200} height={200} alt="pg slot" className={css.flipcardfront} />
               </div>
-              <div className={css.flipcardback}>
-                <h1>John Doe</h1>
-                <p>Architect &amp; Engineer</p>
-                <p>We love that guy</p>
-              </div>
+              <a href="https://watafak88.com/#/playgame" target="_blank" rel="noopener noreferrer">
+                <div className={css.flipcardback}>
+                  <h1>เล่นเกมส์</h1>
+                </div>
+              </a>
             </div>
           </div>
         </div>
@@ -39,14 +45,13 @@ export default function Card() {
           <div className={css.flipcard}>
             <div className={css.flipcardinner}>
               <div className={css.flipcardfront}>
-                <img src="https://watafak88.com/img/promotion/pgslot_11-500.jpg" alt="pg slot" width="100%" height="100%" style={{ borderRadius: "30px" }} />
-
+                <Image src={pgslot3} width={200} height={200} alt="pg slot" className={css.flipcardfront} />
               </div>
-              <div className={css.flipcardback}>
-                <h1>John Doe</h1>
-                <p>Architect &amp; Engineer</p>
-                <p>We love that guy</p>
-              </div>
+              <a href="https://watafak88.com/#/credit" target="_blank" rel="noopener noreferrer">
+                <div className={css.flipcardback}>
+                  <h1>เครดิตฟรี</h1>
+                </div>
+              </a>
             </div>
           </div>
         </div>
@@ -55,16 +60,47 @@ export default function Card() {
           <div className={css.flipcard}>
             <div className={css.flipcardinner}>
               <div className={css.flipcardfront}>
-                <img src="https://watafak88.com/img/promotion/pgslot_5-500.jpg" alt="pg slot" width="100%" height="100%" style={{ borderRadius: "30px" }} />
+                <Image src={pgslot4} width={200} height={200} alt="pg slot" className={css.flipcardfront} />
               </div>
-              <div className={css.flipcardback}>
-                <h1>John Doe</h1>
-                <p>Architect &amp; Engineer</p>
-                <p>We love that guy</p>
-              </div>
+              <a href="https://pgslot-free.com" target="_blank" rel="noopener noreferrer">
+                <div className={css.flipcardback}>
+                  <h1>เกมส์ฟรี</h1>
+                </div>
+              </a>
             </div>
           </div>
         </div>
+
+        <div className={css.column}>
+          <div className={css.flipcard}>
+            <div className={css.flipcardinner}>
+              <div className={css.flipcardfront}>
+                <Image src={pgslot5} width={200} height={200} alt="pg slot" className={css.flipcardfront} />
+              </div>
+              <a href="https://watafak88.com/#/download" target="_blank" rel="noopener noreferrer">
+                <div className={css.flipcardback}>
+                  <h2>ดาวน์โหลด</h2>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className={css.column}>
+          <div className={css.flipcard}>
+            <div className={css.flipcardinner}>
+              <div className={css.flipcardfront}>
+                <Image src={pgslot6} width={200} height={200} alt="pg slot" className={css.flipcardfront} />
+              </div>
+              <a href="https://watafak88.com/#/download" target="_blank" rel="noopener noreferrer">
+                <div className={css.flipcardback}>
+                  <h1>บทความ</h1>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+
       </div>
     </div>
   );
--- pages/footer.js
+++ pages/footer.js
@@ -4,13 +4,11 @@ import lineImg from "../public/img/line.svg";
 
 export default function Footer() {
   return (
-    <div style={{ position: "fixed", bottom: "10px", width: "100%", textAlign: "center" }}>
+    <div style={{ bottom: "10px", width: "100%", textAlign: "center" }}>
       <a href="https://www.facebook.com/Watafak88/" style={{ margin: "5px 10px 10px 10px" }} target="_blank" rel="noopener noreferrer">
-        {/* <img style={{ margin: "5px 10px 10px 10px" }} src="/img/facebook.svg" width={50} height={50} alt="pg slot"></img> */}
-        <Image src={faceImg} style={{ margin: "5px 10px 10px 10px" }} width={50} height={50} alt="pg slot" />
+        <Image src={faceImg} width={50} height={50} alt="pg slot" />
       </a>
       <a href="https://lin.ee/bDb5ic7" style={{ margin: "5px 10px 10px 10px" }} target="_blank" rel="noopener noreferrer">
-        {/* <img style={{ margin: "5px 10px 10px 10px" }} src="/img/line.svg" width={50} height={50} alt="pg slot"></img> */}
         <Image src={lineImg} width={50} height={50} alt="pg slot" />
       </a>
     </div>
